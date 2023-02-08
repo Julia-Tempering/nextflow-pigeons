91cd2812ff26fe76eed41175c5a04b95cd8403d4
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-02-07 21:01:36 -0800

    Small run

--- deliverables/performance-regression/rerun.sh
+++ deliverables/performance-regression/rerun.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env Rscript
 require("ggplot2")
 require("dplyr")
+require("scales")
 
-data <- read.csv("aggregated/stats.csv.gz")
+data <- read.csv("aggregated/stats.csv.gz") %>%
+  mutate(date = as.POSIXct(date, origin="1970-01-01", tz="PT")) %>%
+  mutate(time_per_expl = time / 10 / (2^n_rounds))
+
 data %>%
-  ggplot(aes(x = date, y = time)) +
+  filter(n_rounds == 10) %>%
+  ggplot(aes(x = date, y = time_per_expl, colour = in_master)) +
     scale_y_log10() +
-    ylab("Time (s)") + 
-    xlab("Commit time") +
-    geom_line()  + 
+    scale_x_datetime(labels = date_format("%Y-%m-%d")) +
+    ylab("Time (s) per exploration step") + 
+    xlab("Commit date") +
+    geom_point()  + 
     theme_bw()
 ggsave("times.pdf", width = 5, height = 5)
+
+data %>%
+  filter(n_rounds == 10) %>%
+  ggplot(aes(x = date, y = bytes, colour = in_master)) +
+    scale_x_datetime(labels = date_format("%Y-%m-%d")) +
+    ylab("Allocations for 10 rounds (bytes)") + 
+    xlab("Commit date") +
+    geom_point()  + 
+    theme_bw()
+ggsave("allocations-by-commit.pdf", width = 5, height = 5)
+
+data %>%
+  ggplot(aes(x = n_rounds, y = bytes, colour = date)) +
+    ylab("Allocations (bytes)") + 
+    xlab("Round") +
+    geom_point()  + 
+    theme_bw()
+ggsave("allocations-by-round.pdf", width = 5, height = 5)
